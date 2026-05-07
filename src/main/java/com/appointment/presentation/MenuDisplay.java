56c70c38f11d11f5e3098a548a48f60a5bad387f
awwadaa <awwad3905@gmail.com>
2026-05-07 19:07:56 +0300

    Refactoring MenuDisplay.java

--- src/main/java/com/appointment/presentation/MenuDisplay.java
+++ src/main/java/com/appointment/presentation/MenuDisplay.java
@@ -1,26 +1,36 @@
 package com.appointment.presentation;
 
+import java.util.logging.Logger;
+
 /**
  * Handles displaying menus to the user.
- * 
+ *
  * @author awwadaa
  * @version 1.0
  */
 public class MenuDisplay {
 
+    private static final Logger LOGGER = Logger.getLogger(MenuDisplay.class.getName());
+
+    private static final String MAIN_MENU = String.join(
+            System.lineSeparator(),
+            "",
+            "===== Appointment Scheduling System =====",
+            "1. Administrator Login",
+            "2. View Available Slots",
+            "3. Book Appointment",
+            "4. Cancel Appointment",
+            "5. Modify Appointment",
+            "6. Send Reminder",
+            "7. Administrator Logout",
+            "0. Exit",
+            "Choose an option:"
+    );
+
     /**
      * Displays the main menu.
      */
     public void displayMainMenu() {
-        System.out.println("\n===== Appointment Scheduling System =====");
-        System.out.println("1. Administrator Login");
-        System.out.println("2. View Available Slots");
-        System.out.println("3. Book Appointment");
-        System.out.println("4. Cancel Appointment");
-        System.out.println("5. Modify Appointment");
-        System.out.println("6. Send Reminder");
-        System.out.println("7. Administrator Logout");
-        System.out.println("0. Exit");
-        System.out.print("Choose an option: ");
+        LOGGER.info(MAIN_MENU);
     }
 }
